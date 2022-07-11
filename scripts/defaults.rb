094c10eb12bc4ec062f68fd07701c2662195ead9
Krunal <kpamkar@gmail.com>
2022-07-11 21:25:49 +0530

    Added more functionality

--- scripts/defaults.rb
+++ scripts/defaults.rb
@@ -1,4 +1,9 @@
 module Defaults
+  I3_CONFIG_DIR = File.join(ENV['HOME'], '.config', 'i3')
+  RB_SCRIPTS_DIR = File.join(I3_CONFIG_DIR, 'scripts') 
+  YAML_PATH = File.join(I3_CONFIG_DIR, 'config.yaml')
+  THEMES_DIR = File.join(I3_CONFIG_DIR, 'themes')
+
   I3BAR_DEFAULTS = { 
     cmd: 'i3bar',
     status_cmd: 'i3status --config ~/.config/i3/status_config',
@@ -12,9 +17,9 @@ module Defaults
     w_width: '1',
     bind: 'yes',
     colors: {
-      bg: '#002b36',
-      fg: '#839496',
-      sep: '#073642',
+      bg: ['#002b36'],
+      fg: ['#839496'],
+      sep: ['#073642'],
 
       # param n = 3 <border> <bg> <fg>
       f_w: ['#073642', '#002b36', '#839496'],
@@ -52,6 +57,43 @@ module Defaults
     },
   }
 
+  OPTIONS = { 
+    focus_follows_mouse: 'no',
+    mouse_warping: 'output',
+    focus_wrapping: 'yes',
+    workspace_auto_back_and_forth: 'yes',
+    focus_on_window_activation: 'none',
+    show_marks: 'yes',
+    :"ipc-socket" => "~/.config/i3/i3.socket",
+    default_border: 'pixel 3',
+    default_floating_border: 'pixel 3',
+    hide_edge_borders: 'smart',
+  }
+
+  KBD = {
+    'M:Return' => 'exec --no-startup-id xfce4-terminal',
+    'MC:f' => 'exec --no-startup-id firefox',
+  }
+
+  EXEC = [ 
+    'dex -ae i3 -s ~/.config/autostart',
+  ]
+
   WINDOW_TR = {
+    f_c: 'client.focused',
+    i_c: 'client.focused_inactive',
+    u_c: 'client.unfocused',
+    urg_c: 'client.urgent',
+    bg_c: 'client.background'
+  }
+
+  WINDOW_DEFAULTS = { 
+    # <border> <bg> <fg> <indicator> <child_border>
+    f_c: ['#dc322f', '#002b36', '#b58900', '#268bd2', '#002b36'],
+    i_c: ['#073642', '#073642', '#93a1a1', '#073642', '#073642'],
+    u_c: ['#073642', '#073642', '#657b83', '#073642', '#073642'],
+    urg_c: ['#d33682', '#d33682', '#002b36', '#d33682', '#d33682'],
+    bg_c: ['#002b36'],
   }
 end
+
--- scripts/parser.rb
+++ scripts/parser.rb
@@ -7,22 +7,29 @@ module Parser
     c !~ /^#?[0-9a-fA-F]{6}/ and raise "Invalid hex: #{c}"
   end
 
-  def parse(conf, s:[], d:0, tr: false)
-    tr == false and raise 'Translation table not provided'
-    s.length == 0 && conf.merge!(I3BAR_DEFAULTS)
+  def parse(conf, s:[], d:0, tr: false, default: false)
+    tr = tr || {}
+    default = default || {}
+    conf = default.merge conf
     spaces = d > 0 ? '    ' * d : ''
 
     conf.each { |k,v| 
       _k = k
       k = tr.has_key?(k) ? tr[k] : k
+      default_v = default[k] || []
 
-      if k.is_a? Hash
+      if block_given?
+        out = yield [k, v]
+        s.push out
+      elsif k.is_a? Hash
         s.push "#{spaces}#{_k} {\n"
         parse(v, s: s, d:d+1, tr: k)
         s.push "#{spaces}}"
       else
         k = k.to_s
         if v.is_a? Array
+          puts default_v
+          v = v + (default_v.slice(v.length, default_v.length) || [])
           v = v.map {|c| 
             c.strip!
             check_color c
@@ -38,13 +45,33 @@ module Parser
     s
   end
 
-  def parse_i3bar_conf(conf)
-    s = parse(conf, tr: I3BAR_TR, d:1)
+  def parse_options(opts={})
+    parse(OPTIONS.merge(opts)).join "\n"
+  end
+
+  def parse_exec(cmds=[])
+    (EXEC + cmds).join "\n"
+  end
+
+  def parse_i3bar(conf={})
+    s = parse(conf, tr: I3BAR_TR, d:1, default: I3BAR_DEFAULTS)
     s[0] = "bar {\n#{s[0]}"
     s.push "\n}"
     s.join "\n"
   end
+
+  def parse_window_colors(conf={})
+    parse(conf, tr: WINDOW_TR, default: WINDOW_DEFAULTS)
+  end
+
+  def parse_keybindings(conf={})
+    # M = Super
+    # C = Control
+    # S = Shift
+    # A = Alt/Meta
+    # spec: {Modifier}:{keys}
+  end
 end
 
 include Parser
-puts parse_i3bar_conf({})
+puts parse_window_colors
